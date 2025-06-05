ea279c24a2d10290d491994bcea437b78a60fde2
obulka <oleg.bulka@gmail.com>
2025-06-05 13:16:14 +0300

    Add scaffolds template for nginx with docker-compose, update cli.py

--- devlaunch/cli.py
+++ devlaunch/cli.py
@@ -1,21 +1,34 @@
 import typer
+import os
+import subprocess
 
 app = typer.Typer()
 
-
-@app.command()
-def list():
-    typer.echo("Availavle templates: ...")
+DEFAULT_SCAFFOLDS_DIR = os.path.join(os.getcwd(), "templates", "scaffolds")
 
 
 @app.command()
-def create(name: str):
-    typer.echo(f"Creating {name}...")
+def list(path: str = DEFAULT_SCAFFOLDS_DIR):
+    if not os.path.isdir(path):
+        print(f"No such directory: {path}")
+        return
+
+    for name in os.listdir(DEFAULT_SCAFFOLDS_DIR):
+        full_path = os.path.join(DEFAULT_SCAFFOLDS_DIR, name)
+        if os.path.isdir(full_path):
+            print("-", name)
 
 
 @app.command()
-def deploy():
-    typer.echo("Deploying...")
+def up(name: str):
+    """Підняти шаблон"""
+    compose_path = os.path.join(
+        DEFAULT_SCAFFOLDS_DIR, name, "docker-compose.yml")
+    if not os.path.exists(compose_path):
+        print(f"[!] Template '{name}' doesn't have docker-compose.yml")
+        raise typer.Exit(code=1)
+
+    subprocess.run(["docker-compose", "-f", compose_path, "up", "-d"])
 
 
 if __name__ == "__main__":
