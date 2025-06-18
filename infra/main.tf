57f7643e8c212528afa9ce28e42a52708ed2b013
obulka <oleg.bulka@gmail.com>
2025-06-18 17:35:15 +0300

    Add ingress rule for API port 5000 in security groups

--- infra/main.tf
+++ infra/main.tf
@@ -119,6 +119,13 @@ resource "aws_security_group" "ec2_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
